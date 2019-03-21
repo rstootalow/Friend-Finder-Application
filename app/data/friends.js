ba6cfad1f4cea576706d08988281af812bcc7621
rstutalo <richardtutalo@gmail.com>
2019-03-21 00:26:16 -0600

    added dummy data characters in the database

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+// create 10 different users of the app that you can potentially match with. 
+
+var friend_data = [
+    {
+        "name": "Billy",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2011/08/03/03_billymad.w1200.h630.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Chris",
+        "photo": "https://www.popoptiq.com/wp-content/uploads/2015/07/chris_farley_72221.jpg",
+        "scores": [
+            "3",
+            "1",
+            "5",
+            "5",
+            "2",
+            "1",
+            "1",
+            "2",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Vince",
+        "photo": "https://i.pinimg.com/736x/7d/98/9d/7d989d1b4eb44e20d612b6c8c4d704dc.jpg",
+        "scores": [
+            "5",
+            "4",
+            "3",
+            "3",
+            "3",
+            "1",
+            "1",
+            "2",
+            "2",
+            "1"
+        ]
+    },
+    {
+        "name": "Stephen",
+        "photo": "https://cdn.ebaumsworld.com/mediaFiles/picture/342756/80722405.jpg",
+        "scores": [
+            "1",
+            "1",
+            "2",
+            "1",
+            "1",
+            "2",
+            "2",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Kyle",
+        "photo": "http://worldwideinterweb.com/wp-content/uploads/2016/09/funny-back-to-school-pics.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "3",
+            "2",
+            "4",
+            "2"
+        ]
+    },
+    {
+        "name": "Kevinn",
+        "photo": "https://i.imgur.com/bjch48K.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Kelly",
+        "photo": "http://www.freakingnews.com/pictures/118500/De-Forest-Kelley-with-a-Big-Nose--118841.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Jamie",
+        "photo": "https://farm4.static.flickr.com/3948/14948201174_f3914d0fdb_b.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Emily",
+        "photo": "https://cdn9.dissolve.com/p/D929_41_013/D929_41_013_1200.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    },
+    {
+        "name": "Peter",
+        "photo": "https://www.lifewithdogs.tv/wp-content/uploads/2016/07/7.15.16-Awkward-Family-Photos4.jpg",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "1",
+            "1",
+            "3",
+            "1",
+            "4",
+            "5",
+            "1"
+        ]
+    }
+]
