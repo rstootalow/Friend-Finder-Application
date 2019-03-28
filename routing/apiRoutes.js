0fa13eedbfed8d5179bfb6d54e89d03992df79ed
rstutalo <richardtutalo@gmail.com>
2019-03-28 12:25:43 -0600

    all functionality is working. Need to find a way to populate correct image

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,7 +11,7 @@ var friend_data = require("../app/data/friends.js");
 //EXPORT THE API ROUTES FROM OTHER FILES
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
-        return res.json(friends);
+        return res.json(friend_data);
     });
 
     // ========== POST NEW USER FROM SURVEY ==========
@@ -22,34 +22,30 @@ module.exports = function(app) {
         var userScores = userData.scores;
 
         //object to store best match
-        var compatibleMatch = {
-            name: "",
-            photo: "",
-            diffScore: 100
-        };
-
-        //set total diff to zero and add user scores to it through the loop
-        var totalDiff = 0;
+       var matchName = "";
+       var matchImage = "";
+       //set total diff to zero and add user scores to it through the loop
+        var totalDiff = 100;
 
         //loop through all friends in database 
         for (var i = 0; i < friend_data.length; i++) {
-            console.log(friend_data[i].name);
-            totalDiff = 0;
+            // console.log(friend_data[i].name);
+            var diff = 0;
 
-            for (var j = 0; j <friend_data[i].score[j]; j++) {
-                totalDiff += Math.abs(parseInt(userScores[j] - parseInt(friend_data[i].score[j])));
+            for (var j = 0; j < userScores.length; j++) {
+                diff += Math.abs(parseInt(userScores[j] - parseInt(friend_data[i].scores[j])));
 
-                if (totalDiff <= compatibleMatch.diffScore) {
-                    compatibleMatch.name = friend_data[i].name,
-                    compatibleMatch.photo = friend_data[i].photo,
-                    compatibleMatch.diffScore = totalDiff;
+                if (diff < totalDiff) {
+                    totalDiff = diff;
+                    matchName= friend_data[i].name;
+                    matchImage = friend_data[i].photo;
                 }
             }
         }
         //push data back in to userData object
         friend_data.push(userData);
         //then return a json object with the most compatible match
-        res.json(compatibleMatch);
+        res.json({matchName: matchName, matchImage: matchImage});
 
     });
 };
